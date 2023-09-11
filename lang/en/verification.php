6796b33755b479edfa4560d8cf376e1f8d5e253a
sebayadigital <sebayadigital@gmail.com>
2023-09-11 09:20:01 +0700

    Coba Update

--- lang/en/verification.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'subject' => 'Verifikasi Email',
-    'greeting' => 'Halo!',
-    'message' => 'Silakan klik tombol di bawah ini untuk memverifikasi alamat email Anda:',
-    'button' => 'Verifikasi Email',
-    'salutation' => 'Hormat kami,',
-];
--- resources/views/components/application-logo.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="text-center">
-    <center>
-        <img src="{{ asset($service['data_konfig']->logo) }}" alt="" width="80px"><br>
-        <strong style="font-size: large;">{{ $service['data_konfig']->nama_sistem }}</strong><br>
-        <p>Selamat datang di {{ $service['data_konfig']->nama_instansi }}</p>
-    </center>
-</div>
